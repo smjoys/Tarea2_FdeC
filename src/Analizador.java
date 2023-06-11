64533784d5aaf82497b0836409dbaaebc4df990c
smjoys <iefh00@gmail.com>
2023-06-11 15:21:05 -0400

    9

--- src/Analizador.java
+++ src/Analizador.java
@@ -1,11 +1,12 @@
-import java.io.File;
-import java.io.FilenameFilter;
+import java.io.*;
+import java.time.LocalTime;
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class Analizador {
 
 
-    public ArrayList<Subtitulo> Subtitulos= new ArrayList<Subtitulo>();
+    public ArrayList<Subtitulo> Subs= new ArrayList<Subtitulo>();
 
     public static void buscarArchivoSubRip(File ruta, String arch) {
 
@@ -20,12 +21,49 @@ public class Analizador {
             if (subtitleFiles != null && subtitleFiles.length > 0) {
                 System.out.println("Archivo .srt encontrado:");
                 System.out.println(subtitleFiles[0].getName());
-                /////////////////
+                lectorArchivoSubRip(subtitleFiles[0]);
             } else {
                 System.out.println("No se encontró el archivo .srt en la carpeta especificada.");
             }
         } else {
             System.out.println("La carpeta especificada no existe o no es una carpeta válida.");
         }
+    }
+    public static void lectorArchivoSubRip(File file){
+        FileReader fileReader;
+        //lo meti todo en este try/catch por que si
+        try {
+            fileReader = new FileReader(file);
+            BufferedReader reader = new BufferedReader(fileReader);
+            String line;
+            //LinkedList<String> List = new LinkedList<>();
+            StringBuilder subtitleContent = new StringBuilder();
+            while ((line = reader.readLine()) != null) {
+                subtitleContent.append(line).append("\n");
+            }
+            reader.close();
+            String[] subtitles = subtitleContent.toString().split("\n\n");
+            for (String subtitle:subtitles) {
+                //cada linea del archivo
+                String[] lines = subtitle.split("\n");
+                //numero de subtitulos
+                int numberSubtitle = Integer.parseInt(lines[0]);
+                String[] intervalosTime = lines[1].split(" --> ");
+                LocalTime start = LocalTime.parse(intervalosTime[0]);
+                LocalTime finish = LocalTime.parse(intervalosTime[1]);
+                //los subtitulos en si PD aun no se como separar los otros factores
+                StringBuilder subtitleContentBuild = new StringBuilder();
+                for (int i = 2; i < lines.length; i++) {
+                    subtitleContentBuild.append(lines[i]).append("\n");
+                }
+                String contenidoNeto = subtitleContentBuild.toString().trim();
+                //por fin el subtitulo creado
+                Subtitulo subtituloNodo = new Subtitulo(numberSubtitle,start,finish,contenidoNeto);
+            }
+
+        } catch (IOException e) {
+            System.out.printf("archivo no funcional");;
+        }
+
     }
 }
