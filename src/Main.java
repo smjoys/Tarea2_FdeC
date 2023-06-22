22d02522835dcfc31a3062fffa7d680a56057d5c
MatiasFlores <mafloresgu@gmail.com>
2023-06-21 22:32:48 -0400

    2

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+import java.io.*;
+
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+    public static void main(String []args) {
+
+
+    Scanner tcld = new Scanner(System.in);
+        System.out.println("Ingrese ruta de los subtitulos(C:/ruta/a/buscar):");
+    String ruta = tcld.next();
+        System.out.println("Ingrese nombre del archivo:");
+    String arch = tcld.next();
+
+        Analizador.buscarArchivoSubRip(new File(ruta),arch);
+}
+
 }
