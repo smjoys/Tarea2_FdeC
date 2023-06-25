5f35bb9d053b69adc4d69773e90d46ed6f490fc4
MatiasFlores <mafloresgu@gmail.com>
2023-06-25 17:33:05 -0400

    3

--- src/Gramatica/srt/sableccCarpeta/Analizador.java
+++ src/Gramatica/srt/sableccCarpeta/Analizador.java
@@ -1,6 +1,6 @@
+package Gramatica.srt.sableccCarpeta;
 import java.io.*;
 import java.util.ArrayList;
-
 
 
 public class Analizador {
@@ -71,6 +71,39 @@ public class Analizador {
         } catch (IOException e) {
             System.out.printf("archivo no funcional");;
         }
+
+    }
+    public class Subtitulo {
+
+        int num;
+        String InicialTime;
+        String finalTime;
+        String texto;
+
+        public Subtitulo(int num, String inicialTime, String finalTime, String texto) {
+            this.num = num;
+            this.InicialTime= inicialTime;
+            this.finalTime = finalTime;
+            this.texto = texto;
+        }
+
+
+        public int getNum() {
+            return num;
+        }
+
+        public String getInicialTime() {
+            return InicialTime;
+        }
+
+        public String getFinalTime() {
+            return finalTime;
+        }
+
+        public String getTexto() {
+            return texto;
+        }
+
 
     }
 }
